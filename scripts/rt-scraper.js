abab4f3ce79ed8660bedde7970ad1b3fff55b3f8
Jerry VanStrydonck <jvanstry@gmail.com>
2014-08-12 21:59:19 -0400

    rate limiting prevention of RT requests

--- scripts/rt-scraper.js
+++ scripts/rt-scraper.js
@@ -1,3 +1,33 @@
 var request = require('request');
-var cheerio = require('cheerio');
+var rtApiKey = require('../rt-api-key');
+var save = require('./save-movies');
 
+module.exports = function(movieTitles){
+  timeoutLoop(0, movieTitles);
+};
+
+function urlBuilder(movieName){
+  var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey='
+    + rtApiKey + '&q=' + movieName.split(' ').join('+') + '&page_limit=1';
+
+  return url;
+};
+
+// Rotten tomatoes limits requests to 5 per second, so this setTimeout is
+//   necessary to pause the requests for a short while each time 5 requests
+//   is made. Also need to check each time if have come to end of movies array
+function timeoutLoop(i, movieTitles){
+  setTimeout(function(){
+    for(var temp = i; i < temp + 5 && i < movieTitles.length; i++){
+      var url = urlBuilder(movieTitles[i]);
+
+      request(url, function(err, rtResponse, data){
+        console.log(data);
+      });
+    };
+
+    if(i < movieTitles.length){
+      timeoutLoop(i, movieTitles)
+    }    
+  }, 1500);
+}
