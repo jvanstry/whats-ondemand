b33ba1caa934e74941a6df314d167ab074dd9bfa
Jerry VanStrydonck <jvanstry@gmail.com>
2014-08-13 23:11:15 -0400

    rotten tomato api module accepts titles arr, outputs title+rating arr

--- scripts/rt-scraper.js
+++ scripts/rt-scraper.js
@@ -2,8 +2,9 @@ var request = require('request');
 var rtApiKey = require('../rt-api-key');
 var save = require('./save-movies');
 
+
 module.exports = function(movieTitles){
-  timeoutLoop(0, movieTitles);
+  var moviesWithRatings = timeoutLoop(0, movieTitles, [], save);
 };
 
 function urlBuilder(movieName){
@@ -16,18 +17,31 @@ function urlBuilder(movieName){
 // Rotten tomatoes limits requests to 5 per second, so this setTimeout is
 //   necessary to pause the requests for a short while each time 5 requests
 //   is made. Also need to check each time if have come to end of movies array
-function timeoutLoop(i, movieTitles){
+function timeoutLoop(i, movieTitles, moviesWithRatings, cb){
   setTimeout(function(){
     for(var temp = i; i < temp + 5 && i < movieTitles.length; i++){
-      var url = urlBuilder(movieTitles[i]);
+      var title = movieTitles[i];
+      var url = urlBuilder(title);
 
       request(url, function(err, rtResponse, data){
-        console.log(data);
+        var json = JSON.parse(data);
+        var movieJson = json.movies[0];
+        var movie = {
+          critics: movieJson.ratings.critics_score,
+          title: movieJson.title,
+          audience: movieJson.ratings.audience_score
+        }
+        
+        moviesWithRatings.push(movie);
+
+        if(moviesWithRatings.length === movieTitles.length){
+          cb(moviesWithRatings);
+        }
       });
     };
 
     if(i < movieTitles.length){
-      timeoutLoop(i, movieTitles)
-    }    
+      timeoutLoop(i, movieTitles, moviesWithRatings, cb)
+    }   
   }, 1500);
 }
