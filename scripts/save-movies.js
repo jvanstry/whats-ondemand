020d7b61814a7830be5acbae35bfbc4a8f793111
Jerry VanStrydonck <jvanstry@gmail.com>
2014-09-09 04:53:26 -0400

    changed location of data file

--- scripts/save-movies.js
+++ scripts/save-movies.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var MOVIE_DATA_FILE = 'data.json';
+var MOVIE_DATA_FILE = '../data.json';
 
 module.exports = function(movies){
   var stringMovies = JSON.stringify(movies);
